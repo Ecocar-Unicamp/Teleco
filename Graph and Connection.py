88d4da37db80155d4736bf928bea43ff49cef579
DanielFrulane <83926303+DanielFrulane@users.noreply.github.com>
2021-07-21 14:33:43 -0300

    Update Graph and Connection.py

--- Graph and Connection.py
+++ Graph and Connection.py
@@ -11,10 +11,11 @@ import time
 #checar sempre se listas vazias - "Identified: PermissionError"
 #ultimos valores  - pode ser ao lado das checks
 #info tempo na barra
+#fazer troca de cores dos botoes na chamada de draw e is over
 
 pygame.init()
 
-x_size_of_window = 1200
+x_size_of_window = 1400
 y_size_of_window = 650
 color_of_screen = (0,128,128)
 
@@ -53,7 +54,6 @@ class button():
         if outline:
             pygame.draw.rect(self.window, (0,0,0), (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
 
-
         pygame.draw.rect(self.window, self.color, (self.x, self.y, self.width, self.height), 0)
 
         if self.text != '':
@@ -77,6 +77,11 @@ message_freezing_button_0 = "Freeze"
 message_freezing_button_1 = "Live Data"
 freezing_button_color = (0,0,255)
 freezing_button = button(window_of_visualization,freezing_button_color,100,140,100,20, message_freezing_button_0)
+
+new_tab_button_color = (100,100,255)
+new_tab_button = button(window_of_visualization,new_tab_button_color,100,200,100,20, "New Tab")
+
+'''fazer funão que faz draw de botoes; tambem pro resto'''
 
 #Information holder for individual graphs
 class infograph():
@@ -120,11 +125,16 @@ def connect():
             serial_COM_port.reset_input_buffer()
             serial_COM_port.write("connect".encode())
             timing = time.time()
-            while not serial_COM_port.in_waiting or (time.time() - timing < 3):
-                pass
-            if serial_COM_port.in_waiting: #'''while in waiting'''##############################################
-                input = serial_COM_port.readline().decode('utf-8').strip()
-                print(input)
+            input = None
+            while (time.time() - timing < 3):#deixar mais simples
+                if serial_COM_port.in_waiting:
+                    input = serial_COM_port.readline().decode('utf-8').strip()
+                    #print(input)
+                    if (input == "begin"):
+                        break
+                    else:
+                        input = None
+            if input:
                 if(input == "begin"): #any other case: serial_COM_port = None
                     list_of_infographs = []
                     biggest_step = None
@@ -138,7 +148,7 @@ def connect():
                             break
                         #receives names and steps for graphs
                         input = serial_COM_port.readline().decode('utf-8').strip()
-                        print(input)
+                        #print(input)
                         if(input == "end"):
                             break
                         lista_input = input.split(";")
@@ -198,24 +208,24 @@ class graph():
         if serial_COM_port:
             self.current_list_of_coordinates = []
             self.current_list_of_values_initial_and_final_positions = []
-            smallest_step_x_graphic_step = self.width/(self.size_of_frame - 1)##############
+            smallest_step_x_graphic_step = self.width/(self.size_of_frame - 1)
 
             for i in range(len(self.list_of_infographs)):
                 self.current_list_of_coordinates.append([])
-                if len(self.list_of_infographs[i].list_of_values) > 1:##########******************
+                if len(self.list_of_infographs[i].list_of_values) > 1:
                     '''
                     # conferir se todos os selfs precisam ser selfs ou algum outro dado precisa ser self
                     # conferir se nao generaliza coisa que se baseia na lista de infog recebida - smallest_step_infograph.step
                     # pos processamento para bordas: y = ax + b, valor max e min
                     # diversos eixos y: abreviação de nome
                     '''
-                    '''ESTA DANDO PROBLEAMA, SAI PRA FORA DA JANELA'''
+                    '''CONFERIR SE NAO VAI SAIR PRA FORA DA JANELA'''
                     relative_step_proportion = smallest_step_infograph.step/self.list_of_infographs[i].step
                     list_of_selected_values = []
                     initial_position_in_list = None
                     final_position_in_list = None
 
-                    if len(self.list_of_infographs[i].list_of_values) <= self.size_of_frame * relative_step_proportion:
+                    if len(self.list_of_infographs[i].list_of_values) < self.size_of_frame * relative_step_proportion:
                         initial_position_in_list = 0
                     else:
                         initial_position_in_list = self.initial_smallest_step_position_in_list * relative_step_proportion
@@ -260,16 +270,128 @@ cursor_position = None
 live_data = True
 running = True
 
+
+checkbox_main_color = (53, 87, 28)
+
+class checkbox:
+    def __init__(self, window, name, x, y, size=20, state=False):
+        self.window = window
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = checkbox_main_color
+
+        self.name = name
+        self.state = state
+
+    def draw(self, outline=None):
+        if outline:
+            pygame.draw.rect(self.window, outline, (self.x - 2, self.y - 2, self.size + 4, self.size + 4), 0)
+
+        pygame.draw.rect(self.window, self.color, (self.x, self.y, self.size, self.size), 0)
+
+        text = font.render(self.name, True, (0, 0, 0))
+        (self.window).blit(text, (self.x + 40, self.y + (self.size / 2 - text.get_height() / 2)))
+
+        if self.state:
+            font2 = pygame.font.SysFont('freesansbold.ttf',int(1.5 * self.size))  #######verificar se parametrizavel
+            text = font2.render("X", True, (0, 0, 0))
+            (self.window).blit(text, (self.x + (self.size / 2 - text.get_width() / 2), self.y + (self.size / 2 - text.get_height() / 2)))
+
+    def cursor_is_over(self, cursor_position):
+        return cursor_is_over(self.x, self.y, self.size, self.size, cursor_position)
+
+    ##########################
+    def is_over(self, pos):
+        if self.x < pos[0] < self.x + self.size:
+            if self.y < pos[1] < self.y + self.size:
+                self.color = self.secondary_color #################permite a troca da cor nao diretamente no main
+                return True
+        self.color = self.main_color
+        return False
+    ##########################
+
+
+tab_main_color = (0,255,0)
+tab_secondary_color = (0,150,0)
+
+# Class for the tabs of diferent infographs. The class holds an index that identifies the tab, a list of the infographs
+# avilable and a list of checkboxes used to selecte wich infographs will be shown. The class has a function to create
+# new tabs and a class to draw the contents of the tab
+class tab:
+    def __init__(self, window, x, y, width, height, text, selected=False):
+        self.window = window
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.color = tab_main_color
+
+        self.selected = selected
+        self.checkboxes = []
+        self.selected_names = []
+        for i in range(len(list_of_infographs)):
+            self.selected_names.append(list_of_infographs[i].name)
+
+        for i in range(len(self.selected_names)):
+            self.checkboxes.append(checkbox(window_of_visualization, self.selected_names[i],
+                                            1200, 40 * i + 100, 20,True))#######################################
+
+    def draw(self, outline=True): # Draw the Button
+        if outline:
+            pygame.draw.rect(self.window, (0,0,0), (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        if self.selected: #isso dá pra ser enxugado
+            self.color = tab_secondary_color
+        else:
+            self.color = tab_main_color
+
+        pygame.draw.rect(self.window, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            text = font.render(self.text, True, (0, 0, 0))
+            (self.window).blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(self.window, (100, 100, 100), (1150, 50, 200, 400), 0) #deixar em f do tamanho abaixo
+            for c in range(len(self.checkboxes)):
+                self.checkboxes[c].draw()
+
+    '''def draw(self):
+        if self.selected:
+            for i in range(len(self.list_of_infographs)):
+                self.checkboxes[i].draw(window, (0, 0, 0))
+                if self.checkboxes[i].state:
+                    if not self.list_of_infographs[i] in self.selected_infographs:
+                        self.selected_infographs.append(self.list_of_infographs[i])
+                elif not self.checkboxes[i].state:
+                    if self.list_of_infographs[i] in self.selected_infographs:
+                        self.selected_infographs.remove(self.list_of_infographs[i])'''
+
+    def cursor_is_over(self, cursor_position):  # Determines if the mouse cursor position in tuple (x,y) is over the button
+        return cursor_is_over(self.x, self.y, self.width, self.height, cursor_position)
+
+list_of_tabs = []
+selected_tab = None
+
+#se x pressionado: list_of_tabs.remove(tal) - checar se tem ao menos uma, garantir que tem alguma selecionada
+
+
+
 while running:
 
     connection_button.draw(window_of_visualization)
     freezing_button.draw(window_of_visualization)
+    new_tab_button.draw(window_of_visualization)
+    for t in list_of_tabs:
+        t.draw(window_of_visualization)
 
     #if serial has received information
     # desconexão inesperada: serial.serialutil.SerialException: ClearCommError failed (PermissionError(13, 'O dispositivo não reconhece o comando.', None, 22))
     if serial_COM_port and serial_COM_port.in_waiting:
         input = serial_COM_port.readline().decode('utf-8').strip()
-        print(input)
+        #print(input)
         lista_input = input.split(";")
         #adds new information to respective infograph
         for i in range(len(list_of_infographs)):
@@ -290,7 +412,6 @@ while running:
                 main_graph.size_of_frame += (-1) * event.y * (1 + int(main_graph.size_of_frame/10)) #proportional growth
                 if main_graph.size_of_frame < minimum_frame_size:
                     main_graph.size_of_frame = minimum_frame_size
-            print(main_graph.size_of_frame)
 
 
         if event.type == pygame.MOUSEBUTTONDOWN and button_pressed == (1,0,0):
@@ -306,6 +427,9 @@ while running:
                     connection_button.color = (0,255,0)
                     connection_button.text = message_connection_button_2
                     main_graph.size_of_frame = 10 * minimum_frame_size #################condicional
+
+                    list_of_tabs.append(tab(window_of_visualization, 100, 240, 100, 20, "Tab 1", True))###################################fazer mais simples
+                    selected_tab = list_of_tabs[0]
                 else:
                     connection_button.color = (255,0,0)
                     if last_text != message_connection_button_30:
@@ -321,6 +445,28 @@ while running:
                     freezing_button.text = message_freezing_button_0
                     live_data = True
 
+            if new_tab_button.cursor_is_over(cursor_position) and len(list_of_tabs):
+                list_of_tabs.append(tab(window_of_visualization,100,list_of_tabs[-1].y + 40,100,20,"Tab " + str(len(list_of_tabs) + 1)))
+
+            for t in range(len(list_of_tabs)):
+                if list_of_tabs[t].cursor_is_over(cursor_position):
+                    selected_tab.selected = False
+                    selected_tab = list_of_tabs[t]
+                    selected_tab.selected = True
+
+            if selected_tab:
+                for c in range(len(selected_tab.checkboxes)):
+                    if selected_tab.checkboxes[c].cursor_is_over(cursor_position):
+                        print(selected_tab.checkboxes[c].state)
+                        if selected_tab.checkboxes[c].state:
+                            selected_tab.selected_names.remove(selected_tab.checkboxes[c].name)
+                        else:
+                            selected_tab.selected_names.append(selected_tab.checkboxes[c].name)
+                        selected_tab.checkboxes[c].state = not selected_tab.checkboxes[c].state
+
+
+            #pra cada guia ve se ta acima:
+            #atualizar guia selecionada: main_graph.list_of_infographs = guia_selecionada.list_of_infographs
 
         if event.type == pygame.KEYDOWN:
             live_data = False
@@ -351,7 +497,14 @@ while running:
         main_graph.initial_smallest_step_position_in_list = 0
 
 
-    main_graph.list_of_infographs = list_of_infographs #substituir por selecionados
+    main_graph.list_of_infographs = []
+    if selected_tab: #########################################simplificar
+        for name in selected_tab.selected_names:
+            for equals in list_of_infographs:
+                if name == equals.name:
+                    main_graph.list_of_infographs.append(equals)
+
+
     main_graph.draw()
     if main_graph.cursor_is_over(cursor_position):
         main_graph.info(cursor_position)
